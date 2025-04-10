0b05b8d408fa7363585277c6c67c085a1e4e7659
ElDemont <stev508ga@gmail.com>
2025-04-10 14:53:20 -0500

    Add files via upload

---
+++ coodigo/CURSO-FUNDAMENTOS-PYTHON/Fundamentos del Lenguaje/matematicas.py
@@ -0,0 +1,15 @@
+# matematicas.py
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: No se puede dividir entre cero"
+    return a / b
